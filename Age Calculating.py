80832f2a9bba49f09158b10fd49b9f68d4c1ff05
rajeshitor <rajeshcpi788@gmail.com>
2022-06-14 23:33:03 +0600

    2nd Update

--- Age Calculating.py
+++ Age Calculating.py
@@ -1,47 +1,43 @@
 from email.utils import localtime
 import time
+from calendar import month
 import calendar
-from calendar import isleap, month
+from datetime import date 
 
-def isLeap(year):
-    if year%4==0 and year%100 !=0 or year % 400 == 0:
-        return True
-    else:
-        return False
-def month_days(month,leap_year):
-    if month in [1,3,5,7,8,10,12]:
+def month_days(month,LeapYear):
+    if month in[1,3,5,7,8,10,12]:
         return 31
     elif month in[2,4,6,9,11]:
         return 30
-    elif month == 2 and leap_year:
+    elif month==2 and LeapYear:
         return 29
-
-    elif month==2 and (not leap_year):
+    elif month==2 and not LeapYear:
         return 28
 
-name=input("Enter Your Name : ")
-year=int(input("Enter Year : "))
+def isLeapYears(year):
+    def isLeap(year):
+        if year%4==0 and year%100 !=0 or year % 400 == 0:
+            return True
+        else:
+            return False
+
 localtime = time.localtime(time.time())
-
-
-month = year * 12 + localtime.tm_mon
-
-begain_year=int(localtime.tm_year)- year
-end_year= begain_year+year
-day=0
-
+name=input("Enter your name : ")
+Birth_Year = int(input("Enter your birth year: "))
+Current_year = date.today().year
+Age = (Current_year - Birth_Year)
+month=Age*12+localtime.tm_mon
+begain_year=int(localtime.tm_year)-Age
+end_year=begain_year+Age
+day=
 for y in range(begain_year,end_year):
-    if (isLeap(y)):
-        day= day + 366
+    if(isLeapYears(y)):
+        day=day+366
     else:
-        day = day + 365
-
+        day=day+365
 day=day+localtime.tm_mday
-
-leap_year = isLeap(localtime.tm_year)
-
+leap_year = isLeapYears(localtime.tm_year)
 for m in range(1, localtime.tm_mon):
     day = day + month_days(m, leap_year)
-
-print("%s's age is %d years or " % (name, year), end="")
+print("%s's age is %d years or " % (name, Age), end="")
 print("%d months or %d days" % (month, day))
